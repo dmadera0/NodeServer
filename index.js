905312ec97d29fcad25002c2c4f309fda692f113
Daniel <dmadera0@gmail.com>
2021-01-21 17:17:51 -0700

    created array

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const express = require("express");
 const app = express();
 
 const port = 4000
+
+const candies = [
+    {id: 1, name: 'Skittles'},
+    {id: 2, name: 'Starburst'},
+    {id: 3, name: 'M&Ms'},
+    {id: 4, name: 'Twix'},
+
+]
 app.get('/',(request, response) =>{
     response.json ({message:"goodby!!!!"})
     });
-app.get('/fun',(request, response) =>{
-response.json ({message:"hello!!!!"})
+app.get('/candies',(request, response) =>{
+response.json (candies)
 });
 
 app.listen(port, ()=>{
