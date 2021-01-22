83c6514029ca536b643b824bab50d5630d096428
Daniel <dmadera0@gmail.com>
2021-01-21 17:00:54 -0700

    new route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const { request } = require("express");
 const express = require("express");
 
 const app = express();
 
 const port = 4000
+app.get('/',(request, response) =>{
+    response.json ({message:"goodby!!!!"})
+    });
+app.get('/fun',(request, response) =>{
+response.json ({message:"hello!!!!"})
+});
 
 app.listen(port, ()=>{
     console.log("we are listening on port 4000")
